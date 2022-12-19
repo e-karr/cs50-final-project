ddcf239bca4adb652f15349fcbb1937a3bbdb8fc
Elizabeth Karr <elizabethkarr@Elizabeths-MacBook-Air.local>
2022-12-19 16:11:31 -0600

    December 19, 2022 4:11pm

---
+++ static/script.js
@@ -0,0 +1,11 @@
+// Run script once DOM is loaded
+document.addEventListener('DOMContentLoaded', function() {
+
+    // dynamic select lists for player registration
+    const selectEvent = document.querySelector("#event-registration");
+
+    selectEvent.addEventListener("change", (event) => {
+        
+    })
+
+});
